8da39333a621a043cf1bf6203aa788221f5fd702
Gabriel Viana <gaa.vianaa@gmail.com>
2024-09-30 11:54:52 -0300

    array e JS externo

---
+++ 03-arrays.js
@@ -0,0 +1,11 @@
+// Decalrando um array
+let alunos = ["comiocudequemtalendo", "Patati", "Patata"]
+
+console.log(alunos);
+
+// Acessando um elemento específico no array
+console.log(`se você quer sorrir é com ${alunos[1]}`);
+console.log(`se você quer brincar é com ${alunos[2]}.`);
+console.log(`se você quer sorrir e brincar ${alunos[1]} ${alunos[2]}`);
+
+
