e7979c104e86d50bd8caedd124c5f5703a7d44eb
Gabriel Viana <gaa.vianaa@gmail.com>
2024-10-02 11:35:54 -0300

    atividades de objetos e arrays no js

--- 04-objetos.js
+++ 04-objetos.js
@@ -24,3 +24,64 @@ const livro = {
     ]
 }
 
+console.log(livro);
+
+// Acesso usando nome da prop. e indice do array desta prop.
+console.log(livro.volumes[1]);
+
+// Exemplo 3: Array de Objetos
+
+const livros = [
+    {
+        titulo: "Harry Potter",
+        autor: "J.K. Rowling"
+    },
+
+    {
+        titulo: "Senhor doa Anéis",
+        autor: "J.R.R. Tolkien"
+    },
+    {
+        titulo: "Ghost Rider",
+        autor: "Neil Peart"
+    }
+
+];
+
+console.log(livros[1].autor);
+
+/* Crie um objeto chamado "aluno" contendo os seguintes dados:
+Nome Completo
+Data de Nascimento
+Lista de telefones (fixo e celular)
+(DESAFIO: use um OBJETO nesta propriedade) Endereço contendo (separadamente): 
+rua
+número
+bairro
+Mostre no console o nome do aluno, o telefone celular e o bairro em que mora. */
+
+const aluno = {
+    nomeCompleto: "Gabriel Viana Paulino",
+    datadeNascimento: "05/01/2006",
+    listadetelefones: [
+        "25629292",
+        "11 978360876"
+    ],
+
+    endereco: {
+        rua: "Rua José Borges Do Canto",
+        numero: "20, D",
+        bairro: "Jardim Camargo Novo"
+    }
+}
+
+console.log(`Nome: ${aluno.nomeCompleto}`); 
+console.log(`Telefone: ${aluno.listadetelefones[0]}`);
+console.log(`Celular: ${aluno.listadetelefones[1]}`);
+console.log(`Bairro: ${aluno.endereco.bairro}`);
+
+
+
+
+
+
